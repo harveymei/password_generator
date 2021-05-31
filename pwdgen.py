a7a7ba6627b2e9043cc39fa55fb5027570006416
Harvey Mei <harvey.mei@msn.com>
2021-05-31 10:15:13 +0800

    Commit Message

--- pwdgen.py
+++ pwdgen.py
@@ -21,13 +21,26 @@ numbers = [i for i in string.digits]  # 依次将0-9放入列表
 lowercase_letters = [j for j in string.ascii_lowercase]  # 依次将a-z放入列表
 uppercase_letters = [m for m in string.ascii_uppercase]  # 依次将A-A放入列表
 symbols = [n for n in string.punctuation]  # 依次将符号放入列表
+print(symbols)
 
 
-# 密码复杂度等级
 level1 = numbers
 level2 = numbers + lowercase_letters
 level3 = numbers + lowercase_letters + uppercase_letters
 level4 = numbers + lowercase_letters + uppercase_letters + symbols
+
+level = input("")
+# 密码复杂度等级
+if level_input = '1':
+    level = level1
+elif level_input = '2':
+    level = level2
+elif level_input = '3':
+    level = level3
+elif level_input = '4'
+    level = level4
+
+
 
 print("Numbers include: \n", numbers)
 print("Lowercase Letters include: \n", lowercase_letters)
@@ -51,7 +64,7 @@ print(level4[random.randrange(0, len(level4))])
 level_n = ['level1', 'level2', 'level3', 'level4']
 
 
-def password_generator(level_n, length=12):
+def password_generator(level_4, length=12):
 
     output = []
     while length > 0:
@@ -63,3 +76,10 @@ def password_generator(level_n, length=12):
 
 
 password_generator()
+
+"""
+用户操作
+"""
+input("请选择密码复杂度等级：")
+
+input("请输入生成密码数量：")
