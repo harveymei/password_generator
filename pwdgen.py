9c2a62c749d04a3433ef1f7f045440fa27333c1c
Harvey Mei <harvey.mei@msn.com>
2021-05-31 11:18:02 +0800

    Commit Message

--- pwdgen.py
+++ pwdgen.py
@@ -10,6 +10,7 @@
 Python 密码生成器
 Python Password Generator
 指定长度和复杂度的密码批量生成工具
+用户录入作为参数，传入函数，生成密码
 """
 
 import string
@@ -17,35 +18,41 @@ import random
 
 
 # 密码字符类型
-numbers = [i for i in string.digits]  # 依次将0-9放入列表
-lowercase_letters = [j for j in string.ascii_lowercase]  # 依次将a-z放入列表
-uppercase_letters = [m for m in string.ascii_uppercase]  # 依次将A-A放入列表
+# https://docs.python.org/3/library/string.html
+numbers = [i for i in string.digits]  # 依次将数字放入列表
+lowercase_letters = [j for j in string.ascii_lowercase]  # 依次将小写字母放入列表
+uppercase_letters = [m for m in string.ascii_uppercase]  # 依次将大写字母放入列表
 symbols = [n for n in string.punctuation]  # 依次将符号放入列表
-print(symbols)
 
-
+# 不同等级密码字符组合列表
 level1 = numbers
 level2 = numbers + lowercase_letters
 level3 = numbers + lowercase_letters + uppercase_letters
 level4 = numbers + lowercase_letters + uppercase_letters + symbols
 
-level = input("")
+print("1）数字\n"
+      "2）数字+小写字母\n"
+      "3）数字+小写字母+大写字母\n"
+      "4）数字+小写字母+大写字母+符号\n")
+
+level_input = input("请选择密码复杂度等级:（默认为4）")
 # 密码复杂度等级
-if level_input = '1':
+if level_input == '1':
     level = level1
-elif level_input = '2':
+elif level_input == '2':
     level = level2
-elif level_input = '3':
+elif level_input == '3':
     level = level3
-elif level_input = '4'
+elif level_input == '4' or '':
     level = level4
+else:
+    print("Error Input")
+    exit()
 
 
-
-print("Numbers include: \n", numbers)
-print("Lowercase Letters include: \n", lowercase_letters)
-print("Uppercase Letters include: \n", uppercase_letters)
-print("Symbols include: \n", symbols)
+length_input = input("请输入密码长度：（默认为12）")
+if level_input == '':
+    length_input = 12
 
 # 在指定字符组合中取随机字符，循环，直到满足密码长度要求，打印结果
 
@@ -64,7 +71,7 @@ print(level4[random.randrange(0, len(level4))])
 level_n = ['level1', 'level2', 'level3', 'level4']
 
 
-def password_generator(level_4, length=12):
+def password_generator(level=4, length=12):
 
     output = []
     while length > 0:
@@ -77,9 +84,3 @@ def password_generator(level_4, length=12):
 
 password_generator()
 
-"""
-用户操作
-"""
-input("请选择密码复杂度等级：")
-
-input("请输入生成密码数量：")
